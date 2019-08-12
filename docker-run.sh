66dc23ec031f990d4f9f67eab41ad93874787b4c
Chirag Ahuja <cahuja@visteon.com>
2019-08-13 00:41:10 +0530

    Updated the inspect_data notebook

--- docker-run.sh
+++ docker-run.sh
@@ -1,10 +1,10 @@
 if docker run --rm -it --runtime=nvidia \
 -e CUDA_VISIBLE_DEVICES=1 \
---expose=22 \
 -v `pwd`:/ws \
 -v /home/antpc/visteon/deepSpeech/docker_files:/docker_files \
 -v /media/antpc/hdd/:/hdd \
 -p 6006:6006 \
+-p 8888:8888 \
 --workdir /ws \
 --name deepspeech_specaugment \
 --ipc host \

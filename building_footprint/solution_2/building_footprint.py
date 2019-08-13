3fce21dbcd40caaabb1180767ca1a4ae097035c7
Chirag Ahuja <cahuja1992@gmail.com>
2019-08-13 05:32:48 +0000

    Inspect the output and setting the confidence values

--- building_footprint/solution_2/building_footprint.py
+++ building_footprint/solution_2/building_footprint.py
@@ -73,10 +73,10 @@ class CustomConfig(Config):
     NUM_CLASSES = 1 + 1  # Background + toy
 
     # Number of training steps per epoch
-    STEPS_PER_EPOCH = 100
+    STEPS_PER_EPOCH = 500
 
     # Skip detections with < 90% confidence
-    DETECTION_MIN_CONFIDENCE = 0.9
+    DETECTION_MIN_CONFIDENCE = 0.7
 
 
 ############################################################
